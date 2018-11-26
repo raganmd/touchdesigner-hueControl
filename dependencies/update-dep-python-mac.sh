33fc667e341b90c6cfa30d1cbfb65dfc07baab48
Matthew Ragan <matthewragan@Matthews-MacBook-Pro.local>
2018-11-25 16:30:57 -0800

    updates to .sh file

--- dependencies/update-dep-python-mac.sh
+++ dependencies/update-dep-python-mac.sh
@@ -1,7 +1,16 @@
-:: Update dependencies
+#!/bin/bash 
 
-:: make sure pip is up to date
-python -m pip install --upgrade pip
+# fix up pip with python3
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3 get-pip.py
 
-:: pull phue
-pip install --target="%~dp0\python" phue
+# Update dependencies
+
+# make sure pip is up to date
+python3 -m pip install --upgrade pip
+
+# change current direcotry to where the script is run from
+pwd
+
+# pull phue
+python3 -m pip install --target="\python" phue
