f5a13bdfc7c8972d1fa96bbe8d10d8a0bef435ed
Matthew Ragan <matthew.ragan@obscuradigital.com>
2018-11-27 10:12:08 -0800

    formatting updates to help iron out some line break challenges.

--- td-modules/base_hueControl/hueControlEXT.py
+++ td-modules/base_hueControl/hueControlEXT.py
@@ -10,10 +10,10 @@ from phue import Bridge
 class Hue:
 	'''
 		This is a sample class.
-		
+
 		This sample class has several important features that can be described here.
-		
-		
+
+
 		Notes
 		---------------
 		Your notes about the class go here
@@ -45,17 +45,17 @@ class Hue:
 	def Hue_lights(self):
 		'''
 			This is a sample method.
-			
+
 			This sample method is intended to help illustrate what method docstrings should look like.
-			                
+
 			Notes
 			---------------
 			'self' does not need to be included in the Args section.
-			
+
 			Args
 			---------------
 			None
-									
+
 			Returns
 			---------------
 			None
@@ -68,32 +68,32 @@ class Hue:
 	def Remap_brightness(self, brightness):
 		'''
 			Remap values from 0-1 to 0-255.
-			
+
 			This is a helper function used to convert normalized values to 0-255.
-			                
+
 			Notes
 			---------------
 			'self' does not need to be included in the Args section.
-			
+
 			Args
 			---------------
 			brightness (float)
-			> A normalized float value to be converted to 0-255 
-						
+			> A normalized float value to be converted to 0-255
+
 			Examples
 			---------------
 			self.Remap_brightness(1)
-			
+
 			Returns
 			---------------
 			new_brightness (int)
 			> An interger value of 0 - 255
 		'''
 
-		new_brightness 		= int(tdu.remap(brightness, 
-											self.Brightness_from[0], 
-											self.Brightness_from[1], 
-											self.Brightness_to[0], 
+		new_brightness 		= int(tdu.remap(brightness,
+											self.Brightness_from[0],
+											self.Brightness_from[1],
+											self.Brightness_to[0],
 											self.Brightness_to[1]))
 
 		return new_brightness
@@ -101,19 +101,19 @@ class Hue:
 	def Convert_color(self, rgb):
 		'''
 			A hue color conversion method.
-			
-			This method is used to transform color from rgb values to hue xy 
+
+			This method is used to transform color from rgb values to hue xy
 			values. This follows the hue API for color space conversion.
-			                
+
 			Notes
 			---------------
 
-			
+
 			Args
 			---------------
 			rgb (list)
 			> A list of rgb values to be converted into the xy color space for the hue
-			
+
 			Examples
 			---------------
 			color 		= [1.0, 0.0, 0.0]
@@ -147,18 +147,18 @@ class Hue:
 	def Add_pars_for_lights(self):
 		'''
 			Set-up function for getting the lighting parameters ready with a new device.
-			
+
 			This method loops through the lights that are reported by a given hue Bridge
 			and creates custom paramaters on the parent() component.
-			                
+
 			Notes
 			---------------
 			None
-			
+
 			Args
 			---------------
 			None
-									
+
 			Returns
 			---------------
 			None
@@ -191,7 +191,7 @@ class Hue:
 			parent().pars('Lightcolor{}r'.format(each_light[0]))[0].val = default_color[0]
 			parent().pars('Lightcolor{}g'.format(each_light[0]))[0].val = default_color[1]
 			parent().pars('Lightcolor{}b'.format(each_light[0]))[0].val = default_color[2]
-			
+
 			# add a brightness control par
 			bri_name 		= 'Lightbri{}'.format(each_light[0])
 			bri_label 		= 'Light {} Brightness'.format(each_light[0])
@@ -235,18 +235,18 @@ class Hue:
 	def Clear_hue_lights(self):
 		'''
 			A helper function for setting-up the custom parameters.
-			
+
 			This function is used to clear out all of the custom parameters
 			on the "Individual Lights" page.
-			                
+
 			Notes
 			---------------
 			None
-			
+
 			Args
 			---------------
 			None
-									
+
 			Returns
 			---------------
 			None
@@ -267,19 +267,19 @@ class Hue:
 	def Clear_and_setup_lights(self):
 		'''
 			A helper function for setting-up the custom parameters.
-			
+
 			This combines two other helper functions to correctly clear all
 			previous custom parameters, and create new pars for all lights
 			connected to the current Hue Bridge.
-			                
+
 			Notes
 			---------------
 			None
-			
+
 			Args
 			---------------
 			None
-									
+
 			Returns
 			---------------
 			None
@@ -292,21 +292,21 @@ class Hue:
 	def Build_command_dict(self, light_index):
 		'''
 			A helper function for building a command dictionary for lights.
-			
+
 			In the phue library there's a function available for setting lights
 			based on their name. This function looks to the index of the parmeter
 			that's been pulsed, and retrieves all of the relevant information to build
 			a command list.
-			                
+
 			Notes
 			---------------
 			None
-			
+
 			Args
 			---------------
 			light_index (int)
 			> The integer value associated with the custom parameter index for a light
-						
+
 			Examples
 			---------------
 			self.Build_command_dict(1)
@@ -317,10 +317,13 @@ class Hue:
 			> A dictionary with all appropriate keys for using the set_light() command
 		'''
 
+		light_par_trans 	= parent().pars('Lighttrans{}'.format(light_index))[0].eval()
+		color_as_list 		= [chan.eval() for chan in parent().pars('Lightcolor{}*'.format(light_index))]
+
 		on_cmd 				= parent().pars('Lightpwr{}'.format(light_index))[0].eval()
 		bri_cmd 			= parent().pars('Lightbri{}'.format(light_index))[0].eval()
-		trans_cmd 			= 1 if parent().pars('Lighttrans{}'.format(light_index))[0].eval() == 0 else parent().pars('Lighttrans{}'.format(light_index))[0].eval()
-		xy_cmd 				= self.Convert_color([chan.eval() for chan in parent().pars('Lightcolor{}*'.format(light_index))])
+		trans_cmd 			= 1 if light_par_trans == 0 else light_par_trans
+		xy_cmd 				= self.Convert_color(color_as_list)
 
 		command_dict 		= {
 			'on'				: bool(on_cmd),
@@ -333,22 +336,22 @@ class Hue:
 	def Update_light_by_par_index(self, par_index, debug=False):
 		'''
 			This is helper method for sending a set_light() command to a specific hue light.
-			
+
 			This method fetches the appropriate light name, constructs a command list, and then
 			sends a command to the hue bridge to set the specified light appropriately.
-			                
+
 			Notes
 			---------------
 			None
-			
+
 			Args
 			---------------
 			par_index (int)
 			> The integer value associated with the custom parameter index for a light
-			
+
 			debug (bool)
 			> A boolean value for toggling logged output
-			
+
 			Examples
 			---------------
 			self.Update_light_by_par_index(1)
@@ -361,7 +364,7 @@ class Hue:
 		My_bridge 			= Bridge(self.Bridge_ip.eval())
 
 		command_dict 		= self.Build_command_dict(par_index)
-		light_name 			= parent().pars('Lightname{}'.format(par_index))[0].val	
+		light_name 			= parent().pars('Lightname{}'.format(par_index))[0].val
 
 		My_bridge.set_light(light_name, command_dict)
 
@@ -376,19 +379,19 @@ class Hue:
 	def Update_all_by_settings(self):
 		'''
 			Loops through all lights and uses the settings on the Individual Lights page.
-			
+
 			This method uses the settings on the "Individual Lights page to set the pars for
 			all lights. This allows for a single pulse update rather than individual lighting
 			calls per light. This convenience function takes advantage of other helper methods.
-			                
+
 			Notes
 			---------------
 			None
-			
+
 			Args
 			---------------
 			None
-									
+
 			Returns
 			---------------
 			None
@@ -405,15 +408,15 @@ class Hue:
 	def Set_all_lights(self, debug=False):
 		'''
 			Set all Lights to uniform pars.
-			
-			This method uses the parameters set on the "All Lights" page to send commands to all 
+
+			This method uses the parameters set on the "All Lights" page to send commands to all
 			hue lights at the same time. This assumes you would like to control all lights in the
 			same fashion - as a single group.
-			                
+
 			Notes
 			---------------
 			None
-			
+
 			Args
 			---------------
 			None
@@ -423,7 +426,7 @@ class Hue:
 			None
 		'''
 		My_bridge 			= Bridge(self.Bridge_ip.eval())
-		
+
 		transition 			= parent().par.Alltranstime.eval() * self.Trans_time_scaler
 		brightness 			= self.Remap_brightness(parent().par.Allbrightness.eval())
 		rgb 				= self.Convert_color([chan.eval() for chan in parent().pars('Allcolor*')])
@@ -442,7 +445,7 @@ class Hue:
 			each.brightness 		= brightness
 
 		return
-	
+
 	def Threaded_all_lights_worker(self, bridgeIP, pwr, transTime, bri, rbgAsXy):
 
 		My_bridge 			= Bridge(bridgeIP)
@@ -456,8 +459,8 @@ class Hue:
 		return
 
 	def Start_thread(self):
-		# myThread            = threading.Thread(	target=self.Threaded_all_lights_worker, 
+		# myThread            = threading.Thread(	target=self.Threaded_all_lights_worker,
 		# 										args=(My_bridge, pwr, transition, brightness, rgb,))
 		# myThread.start()
-		# 
+		#
 		return
