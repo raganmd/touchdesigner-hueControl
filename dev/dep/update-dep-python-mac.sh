5db124e9fbecb6ce31cbe02bbb499ac73ae19365
Matthew Ragan <matthewragan@Matthews-MacBook-Pro.local>
2018-12-02 13:08:46 -0800

    updates to bash script to correclty work on mac

--- dev/dep/update-dep-python-mac.sh
+++ dev/dep/update-dep-python-mac.sh
@@ -6,6 +6,9 @@ pythonDir=/python
 # change current direcotry to where the script is run from
 dirname "$(readlink -f "$0")"
 
+# permission to run the file
+sudo chmod 755 udpate-dep-python-mac.sh
+
 # fix up pip with python3
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py
